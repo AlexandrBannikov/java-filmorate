76848272a5b30ad121793af063c2c92971aa7976
Alexandr Bannikov <7932417721@mail.ru>
2024-02-08 09:51:18 +0500

    ТЗ9.First commit.(2)

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-
 import java.time.LocalDate;
 
 @Data
 public class User {
+
     private int id;
     @Email
     private String email;
